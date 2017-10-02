df3f17227181acbcb54c2d2db77a0d9784147e95
Md. Nadimozzaman Pappo <mnpappo@gmail.com>
2017-10-02 12:10:08 +0600

    xxxxxx

--- crawler_manager/admin.py
+++ crawler_manager/admin.py
@@ -20,29 +20,6 @@ class CrawelIssuekResource(resources.ModelResource):
 
 @admin.register(models.CrawelIssue)
 class CrawelIssueAdmin(ImportExportModelAdmin, admin.ModelAdmin):
-    
-    # def get_queryset(self, request):
-    #     qs = super(CrawelIssueAdmin, self).get_queryset(request)
-    #     return qs.filter(user=request.user)
-
-    # def has_change_permission(self, request, obj=None):
-    #     if not obj:
-    #         # the changelist itself
-    #         return True
-    #     return obj.user == request.user
-    #def save_model(self, request, obj, form, change):
-    #    obj.user = request.user
-    #    obj.save()
-
-    #def get_queryset(self, request):
-    #    qs = super(CrawelIssueAdmin, self).get_queryset(request)
-    #    return qs.filter(user=request.user)
-
-    #def has_change_permission(self, request, obj=None):
-    #    if not obj:
-            # the changelist itself
-    #        return True
-    #    return obj.user == request.user
 
     resource_class = CrawelIssuekResource
 
@@ -50,17 +27,6 @@ class CrawelIssueAdmin(ImportExportModelAdmin, admin.ModelAdmin):
     list_filter = ['city_name', 'keyword', 'rating']
     search_fields = ['keyword', 'city_name', 'title', 'rating', 'votes', 'address', 'contact', 'website']
     list_per_page = 5000
-
-    def get_queryset(self, request):
-        qs = super(CrawelIssueAdmin, self).get_queryset(request)
-        return qs.filter(created_by=request.user)
-
-    def save_model(self, request, obj, form, change):
-        if not change:
-            # the object is being created, so set the user
-            obj.created_by = request.user
-        obj.save()
-
 
 admin.site.site_header = 'Justdial Scrapper'
 admin.site.site_title = 'Get data from Justdial by city and categories'
