4ca0bcc3afba52a4a6af4190cbc5bea7d2215e8c
jhonedisongomez <jhon_edinson11@hotmail.com>
2017-10-21 12:00:04 -0500

    add adjust to use chrome browser

--- crawler_manager/tasks.py
+++ crawler_manager/tasks.py
@@ -10,6 +10,9 @@ import pandas as pd
 import re
 from datetime import datetime
 from django.http import HttpResponse
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 
 newspaper_base_url = 'https://www.justdial.com/'
 headers = {"User-agent":"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.80 Safari/537.36"}
@@ -43,99 +46,92 @@ def start_srabbing(data, request):
         print (city_name, keyword, numof_pages)
         print ('requested by user id: {}'.format(user))
 
+        driver = webdriver.Chrome('/home/desarrollador/Documentos/.projects/justdial_data_extractor/.env/selenium/webdriver/chromedriver')
+        driver2 = webdriver.Chrome('/home/desarrollador/Documentos/.projects/justdial_data_extractor/.env/selenium/webdriver/chromedriver')   
+        city_name, keyword, numof_pages, user = data[0], data[1], int(data[2]), data[3]
+        example = ""
+        html_markup = {}
+        url = ""
+        print (city_name, keyword, numof_pages)
+        print ('requested by user id: {}'.format(user))
+
         instance_index = 0
-        for i in range(numof_pages):
-            
-            city_page_url = newspaper_base_url + city_name + '/' + keyword + '/' + 'page-' + str(i+1)
-            
-            html_markup = requests.get(check_url(city_page_url), headers=headers)
-            print ('got page url {} with {}'.format(city_page_url, html_markup.status_code))
+        pages_set = 0
 
-            page_soup = BeautifulSoup(html_markup.content, "lxml")
-            try:
-                contents = page_soup.find("ul", {"class" : "rsl col-md-12 padding0"}).find_all("li", {"class" : "cntanr"})
-            except Exception as e:
-                contents = ''
-                pass                
-            for cont in contents:
-                flag = True
-                try:
-                    title = cont.find("a", {"class" : "nlogo lazy srtbyPic" }).get('title')
-                    try:
-                        rating = cont.find("span", {"class" : "exrt_count" }).get_text()
-                    except Exception as e:
-                        rating = ''
-                        pass
-                    try:
-                        votes = cont.find("span", {"class" : "rt_count lng_vote" }).get_text().strip(' \t\n\r')[0:5:1].strip()
-                    except Exception as e:
-                        votes = ''
-                        pass
-                
-                    # enter into the page
-                    single_url = cont.find("a", {"class" : "nlogo lazy srtbyPic" }).get('href')
-                    html_markupx = requests.get(check_url(single_url), headers=headers)
-                    page_soup = BeautifulSoup(html_markupx.content, "lxml")
-                    cc = page_soup.find("div", {"class" : "col-sm-4 col-xs-12 padding0 leftdt"})               
+        for page in range(numof_pages):
+        
+            pages_set = pages_set + 1
+            page = 'https://www.justdial.com/' + city_name + '/' + keyword + '/page-'+ str(pages_set)
+            print(page)
+            driver.get(page)
+            elem = driver.find_element_by_tag_name("body")
 
-                    try:
-                        contact = cc.find_all("div", {"class" : "telCntct cmawht"})
-                        contact = contact[0].get_text() + contact[1].get_text()
-                        print (contact)
-                    except Exception as e:
-                        contact = cc.find("div", {"class" : "telCntct cmawht"}).get_text() # compulsory field to scrab
-                        print ('could not get, got exception')
-                        pass
-                
-                    try:
-                        address = cc.find("span", {"class" : "lng_add"}).get_text()
-                    except Exception as e:
-                        address = ''
-                        pass
-                    try:
-                        website = cc.find("span", {"class" : "mreinfp comp-text"}).find('a').find_next('a').get_text()
-                    except Exception as e:
-                        website = ''
-                        pass
-                
-                
+            no_of_pagedowns = 100
 
-                except Exception as e:
-                    print (e)
-                    pass
-                    flag = False
-            
-                if flag is True:
+            while no_of_pagedowns:
+                elem.send_keys(Keys.PAGE_DOWN)
+                time.sleep(0.2)
+                no_of_pagedowns-=1
+
+            isContact = False
+            for i in driver.find_elements_by_class_name("cntanr"):
+
+                title = i.find_element_by_class_name("srtbyPic").get_attribute("title")
+                rating = i.find_element_by_class_name("exrt_count").get_attribute("innerHTML")
+                votes = i.find_element_by_class_name("lng_vote").get_attribute("innerHTML").strip(' \t\n\r')[0:5:1].strip()
+                single_url = i.find_element_by_class_name("rating_div").get_attribute("href")
+                contacts = ""
+                address = ""
+                website = ""
+                driver2.get(single_url)
+                elem2 = driver2.find_element_by_tag_name("body")
+                elem2.send_keys(Keys.PAGE_DOWN)
+                time.sleep(0.2)
+                for contact in elem2.find_elements_by_class_name("leftdt"):
+                    for tel in contact.find_elements_by_class_name("tel"):
+                        contacts =  contacts + " " + tel.get_attribute("innerHTML")
+                        contacts = contacts.replace("<b>","")
+                        contacts = contacts.replace("</b>","")
+                    print(contacts)
+                    address = contact.find_element_by_class_name("lng_add").get_attribute("innerHTML")
+                    websites = contact.find_elements_by_css_selector(".mreinfp.comp-text")
+                    website = ""
+                    for ix,get_website  in enumerate(websites):
+                        if ix == 1:
                     
-                    """print ( 'No: {}, Saving data to database.'.format(instance_index))
-                    issue = CrawelIssue.objects.create(
-                        user = user,
-                        keyword = keyword, 
-                        city_name = city_name, 
-                        # crawel_number = crawel_number, 
-                        instance_index = instance_index, 
-                        title = title, 
-                        rating = rating, 
-                        votes = votes, 
-                        contact = contact, 
-                        address = address, 
-                        website = website,
-                        )"""
-                    out.writerow({ 'user':user,
+                            website = get_website.find_element_by_tag_name("a").get_attribute("href")
+
+                """issue = CrawelIssue.objects.create(
+                user = user,
+                keyword = keyword, 
+                city_name = city_name, 
+                # crawel_number = crawel_number, 
+                instance_index = instance_index, 
+                title = title, 
+                rating = rating, 
+                votes = votes, 
+                contact = contacts, 
+                address = address, 
+                website = website,
+                )"""
+
+                out.writerow({ 'user':user,
                                   'keyword':keyword,
                                   'city_name':city_name,
                                   'instance_index':instance_index,
                                   'title':title,
                                   'rating':rating,
                                   'votes':votes,
-                                  'contact':contact,
+                                  'contact':contacts,
                                   'address':address,
                                   'website':website})
-                    instance_index += 1
-                    print(str(instance_index) + " - save in the excel ")
 
-    except Exception as e:
-        print(e)    
 
+                print ( 'No: {}, Saving data to database.'.format(instance_index))
+                instance_index += 1       
     
+    except Exception as e:
+        print(e) 
+
+
     return response
